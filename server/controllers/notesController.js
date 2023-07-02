c31f661528b78f76ea8b2045e47671679d40df9c
Doramahoy <ilya.spasskikh@mail.ru>
2023-07-02 21:46:05 +0300

    updated notes req logic

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -1,29 +1,46 @@
 const {Notes} = require('../models/models')
 const ApiError = require('../error/ApiError')
 class NotesController {
-    async createNote(req, res){
-        const {title, description, userID} = req.body;
-        const note = await Notes.create({title, description, userID})
+    async createNote(req, res, next){
+        const {title, description, userId} = req.body;
+        if (!userId) {
+            return next(ApiError.badRequest('id пользователя нет'))
+        }
+        const note = await Notes.create({title, description, userId})
         return res.json(note);
     }
-    async updateNote(req, res){
+    async updateNote(req, res, next){
         const {title, description} = req.body;
-        const {id} = req.query;
-        const note = await Notes.update({title: title, description: description}, {where: {id: id}})
+        const {id, userId} = req.query;
+        if (!userId) {
+            return next(ApiError.badRequest('id пользователя нет'))
+        }
+        const note = await Notes.update({title: title, description: description}, {where: {id: id, userId: userId}})
         return res.json(note);
     }
-    async deleteNote(req, res){
-        const {id} = req.query;
-        const note = await Notes.destroy({where: {id: id, userID: req.query.userID}})
+    async deleteNote(req, res, next){
+        const {id, userId} = req.query;
+        if (!userId) {
+            return next(ApiError.badRequest('id пользователя нет'))
+        }
+        const note = await Notes.destroy({where: {id: id, userID: userId}})
         return res.json(note);
     }
-    async getAll(req, res){
-        const note = await Notes.findAll({where: {userID: req.query.userID}})
+    async getAll(req, res, next){
+        const {userId} = req.query;
+        if (!userId) {
+            return next(ApiError.badRequest('id пользователя нет'))
+        }
+        const note = await Notes.findAll({where: {userId: userId}})
         return res.json(note);
     }
-    async getOne(req, res){
+    async getOne(req, res, next){
         const id = req.params.id;
-        const note = await Notes.findOne({where: {id: id, userID: req.query.userID}})
+        const {userId} = req.query;
+        if (!userId) {
+            return next(ApiError.badRequest('id пользователя нет'))
+        }
+        const note = await Notes.findOne({where: {id: id, userId: userId}})
         return res.json(note);
     }
 }
