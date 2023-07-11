8205c9f4c3397e2a6307403ed04c2809cb0436a9
krancheh <krancheh@gmail.com>
2023-07-11 09:22:47 +0300

    moved server to dif repo

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router();
-const userRouter =require('./userRouter')
-const notesRouter =require('./notesRouter')
-
-router.use('/user', userRouter)
-router.use('/notes', notesRouter)
-
-module.exports = router;
